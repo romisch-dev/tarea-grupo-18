fc79435be7558f2a35178725471cd6411706cc54
Christian Cano <canostein@gmail.com>
2023-08-16 15:14:50 -0400

    agregado producer y consumer

---
+++ producer.py
@@ -0,0 +1,72 @@
+import json
+import traceback
+from datetime import datetime
+
+import websocket
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+
+producer = KafkaProducer(
+    bootstrap_servers="localhost:9092",
+    value_serializer=lambda m: json.dumps(m).encode("ascii"),
+)
+
+topic = "prueba-1"
+
+
+# Redpanda handlers
+def on_success(metadata):
+    print(f"Message produced to topic '{metadata.topic}' at offset {metadata.offset}")
+
+
+def on_error(e):
+    print(f"Error sending message: {e}")
+
+
+# WS handlers
+def on_ws_message(ws, message):
+    data = json.loads(message)["data"]
+    records = [
+        {
+            "symbol": d["s"],
+            "price": d["p"],
+            "volume": d["v"],
+            "timestamp": datetime.utcfromtimestamp(d["t"] / 1000).strftime(
+                "%Y-%m-%d %H:%M:%S"
+            ),
+        }
+        for d in data
+    ]
+
+    for record in records:
+        future = producer.send(topic, value=record)
+        future.add_callback(on_success)
+        future.add_errback(on_error)
+        producer.flush()
+
+
+def on_ws_error(ws, error):
+    print(error)
+
+
+def on_ws_close(ws, close_status_code, close_msg):
+    producer.close()
+    print("### closed ###")
+
+
+def on_ws_open(ws):
+    ws.send('{"type":"subscribe","symbol":"AAPL"}')
+    ws.send('{"type":"subscribe","symbol":"AMZN"}')
+    ws.send('{"type":"subscribe","symbol":"BINANCE:BTCUSDT"}')
+
+
+if __name__ == "__main__":
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp(
+        "wss://ws.finnhub.io?token=cjeep9pr01qgod9am0jgcjeep9pr01qgod9am0k0",
+        on_message=on_ws_message,
+        on_error=on_ws_error,
+        on_close=on_ws_close,
+    )
+    ws.on_open = on_ws_open
+    ws.run_forever()
